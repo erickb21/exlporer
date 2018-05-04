d5c3171d1064b86e10fbde70471dea6c25ba7c86
alexandreb21000 <alexandre.b@codeur.online>
2018-05-04 16:39:41 +0200

    Dernier fichiers mise à jour et ajout des images

--- pages/explorer.php
+++ pages/explorer.php
@@ -7,7 +7,7 @@ if  (isset ($_POST['maselection'] ))
 {
     $myPath = str_replace('\\','/',$_POST['maselection']).'/';
 
-    echo 'PATH : '.$myPath;
+    echo '<div class="path">PATH : '.$myPath .'</div>';
 
 
 
@@ -22,26 +22,62 @@ function returnFiles($pathFiles)
     //$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($pathFiles));
     $it = new RecursiveDirectoryIterator($pathFiles);
     $toto=$it->key();
-    echo '<p>chemin : '.strrpos($toto,"/").'<p>';
-    echo '<p>parent v<7.0 : '.left($toto,strrpos($toto,"/")).'<p>';
+    //echo '<p>chemin : '.strrpos($toto,"/").'<p>';
+    //echo '<p>parent v<7.0 : '.left($toto,strrpos($toto,"/")).'<p>';
     //echo '<p>parent v>7.0: ' .dirname($toto).'</p>';
     $toto2=left($toto,strrpos($toto,"/"));
-    echo '<p onclick="exploreMyFolder(\'' .$toto2.'\');">retour</p>';
+    echo '<div class="back" onclick="exploreMyFolder(\'' .$toto2.'\');"></div>';
 
     while($it->valid()) {
-
         if (!$it->isDot()) {
-
             //echo '<p>SubPathName: ' . $it->getSubPathName() . "\n</p>";
-            if (is_dir($it->key())) {echo '<p class="directory" data-path="'.$it ->key().'" onclick="exploreMyFolder(this.dataset.path);">dossier : ' . $it->getSubPathName() . "\n</p>";}
-            else {echo '<p class="file">fichier : ' . $it->getSubPathName() . "\n</p>";}
+            if (is_dir($it->key())) {echo '<div class="directory" data-path="'.$it ->key().'" onclick="exploreMyFolder(this.dataset.path);"></div><p>'. $it->getSubPathName().'</p>'  ;}
+            else {
 
+                $extention=right($it->getSubPathName(),strlen($it->getSubPathName()) - strrpos($it->getSubPathName(),"."));
+
+                switch ($extention):
+                    case '.html':
+                        $maClass="htm";
+                        break;
+                    case '.htm':
+                        $maClass="htm";
+                        break;
+                    case '.jpg':
+                        $maClass="jpg";
+                        break;
+                    case '.png':
+                        $maClass="png";
+                        break;
+                    case '.psd':
+                        $maClass="psd";
+                        break;
+                    case '.mp3':
+                        $maClass="mp3";
+                        break;
+                    case '.js':
+                        $maClass="js";
+                        break;
+                    case '.css':
+                        $maClass="css";
+                        break;
+                    case '.xml':
+                        $maClass="xml";
+                        break;
+                    case '.php':
+                        $maClass="php";
+                        break;
+                        default:
+                            $maClass="other";
+                     endswitch;
+
+                echo '<div data-typefile=' .$extention .' class="file '. $maClass .'"></div><p>' . $it->getSubPathName() . '</p>';
+            }
             //echo '<p>SubPath:	 ' . $it->getSubPath() . "\n</p>";
             //echo '<p>Key:		 ' . $it->key() . "\n\n</p>";
         }
         $it->next();
     }
-
 }
 
 function left($str, $length) {
